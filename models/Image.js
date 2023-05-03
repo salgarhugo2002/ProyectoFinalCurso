1b9106a349f337dc001e867a8323a05f6983aa3d
ivan02 <ivan.esteban@institutvidreres.cat>
2023-05-03 16:14:58 +0200

    f

---
+++ models/Image.js
@@ -0,0 +1,19 @@
+const {Schema, model} = require('mongoose')
+const imageSchema= new Schema({
+    title: { type: String },
+    description: { type: String },
+    filename: { type: String },
+    path: { type: String },
+    originalname: { type: String},
+    mimetype: { type: String },
+    size: { type: Number},
+    created_at: { type: Date, default: Date.now() }
+
+},
+
+{
+    versionKey: false
+}
+)
+
+module.exports = model('Image', imageSchema)
